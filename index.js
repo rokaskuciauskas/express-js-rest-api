f267c06c63c26b0abc48a99d0d5b4fa58413143e
Rokas Kuciauskas <rokas.kuciauskas@gmail.com>
2019-05-04 15:49:56 +0300

    add upload image functionality

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
-const app = express()
+const multer = require('multer')
 const port = 3000
+const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './uploads')
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage: storage }).single('userPhoto');
+
+app.get('/upload-image', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+app.post('/api/photo', (req, res) => {
+    upload(req, res, (err) => {
+        !!err ? res.end("Upload failed.") : res.end("File is uploaded.")
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
